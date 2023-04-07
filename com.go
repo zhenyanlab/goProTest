ee7ef3658f9e58fe8ad178868ee875c0028dcc8c
sam <shixiaomu2000@gmail.com>
2023-04-07 20:38:43 +0800

    test2
    test2

--- com.go
+++ com.go
@@ -1,4 +1,9 @@
 package ttproject
-func  ModFuncTest  (str string ) string{
-	return "" +str +"|"
+
+func ModFuncTest(str string) string {
+	return "@@@@" + str + "|"
+}
+func Add2(a int, b int) (c int) {
+	c = a + b
+	return
 }
